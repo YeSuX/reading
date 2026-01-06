042f5350f764d6722f9a8c818fee676199c8f09a
YeSuX <351490949@qq.com>
2026-01-06 22:30:03 +0800

    Update layout title and adjust icon positioning in navigation for improved aesthetics in "读书笔记".

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -4,12 +4,8 @@ import { NotebookPen } from "lucide-react";
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: "苏雄的读书笔记",
-      children: (
-        <div className="flex items-center gap-2">
-          <NotebookPen className="size-5" />
-        </div>
-      ),
+      title: "读书笔记",
+      children: <NotebookPen className="size-5 ml-2" />,
     },
   };
 }
